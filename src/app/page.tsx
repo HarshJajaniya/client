88521d30fbba2792889e9b96f18063f220167d37
HarshJajaniya <ajjujajaniya@gmail.com>
2025-12-21 11:28:42 +0530

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import Image from "next/image";
+import Homepage from "@/app/home/page";
 
 export default function Home() {
-  return (
-    <div className="dark:bg-dark-bg-2 flex min-h-screen items-center justify-center bg-zinc-50 text-gray-900 dark:text-gray-100">
-      HI
-    </div>
-  );
+  return <Homepage />;
 }
